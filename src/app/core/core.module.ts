5aeed814931a00a9b71992005cfcb6c00a0b29ff
Mihaela Mesarosiu <mihaela@kokoon.dev>
2020-10-08 18:18:04 +0300

    ngrx effects and store installed with ng

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { environment } from '../../environments/environment';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
     SharedModule,
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production,
-    }),
   ],
 })
 export class CoreModule {};
